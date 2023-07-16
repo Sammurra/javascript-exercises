246256b33a944d4fe9fc29e6f6c6272a9411b53b
cc221033 <cc221033@fhstp.ac.at>
2023-07-16 11:02:59 +0200

    all done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(arr, ...args) {
+    for (let i = 0; i < args.length; i++) {
+      const arg = args[i];
+      const index = arr.indexOf(arg);
+      if (index !== -1) {
+        arr.splice(index, 1);
+      }
+    }
+    return arr;
+  };
+  
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * 5/9;
+  tempRoundUp = Math.ceil(temp * 10) /10;
+  return tempRoundUp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  temp = temp * 9/5 + 32;
+  tempRoundUp = Math.ceil(temp * 10) /10;
+  return tempRoundUp;
 };
 
 // Do not edit below this line
